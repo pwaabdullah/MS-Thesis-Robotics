1b8e31230dbd200c05f70f53b97fcef4d4231fcb
Md. Abdullah Al Mamun <aamcse@gmail.com>
2016-10-30 18:10:17 +0300

    circle 4 wheel

--- Firmware/Modeling/fourWhee2l.m
+++ Firmware/Modeling/fourWhee2l.m
@@ -10,8 +10,28 @@ s = serial('COM6');
 set(s,'BaudRate',38400);
 fopen(s);
 %% direct profile
-A = [0
-     0 
+pause(2.5)
+turn=0;
+set(s, 'TimeOut', 0.1)
+h=[]
+tic
+for i=2:inf
+    pause(1)
+    t(i)=toc;   
+    T=t(i)-t(i-1);
+    vvx = 100;
+    vvy = 0;
+    x(i)=700*sin(t(i)*.1);
+    y(i)=700*cos(.1*t(i));
+    dx(i)=(x(i)-x(i-1))/T;
+    dy(i)=(y(i)-y(i-1))/T;
+%     dx(i)=(3*cos((3*t(i))/5))/10;
+%     dy(i)=-(3*sin((3*t(i))/5))/10;
+%     V(i)=sqrt(dx(i)^2+dy(i)^2);
+%     q(i)=atan2(dy(i),dx(i))*180/pi;%+turn*360;
+plot(x,y)
+    A = [dx(i)
+     dy(i) 
      0];
 D = [-sin(-60*pi/180) cos(-60*pi/180) 1
      -sin(60*pi/180) cos(60*pi/180) 1
@@ -50,6 +70,11 @@ end
 D=strcat (int2str(d1),'.',int2str(abs(v(1))),'.',int2str(d2),'.',int2str(abs(v(2))),'.',int2str(d3),'.',int2str(abs(v(3))),'.',int2str(d4),'.',int2str(abs(v(4))))
 
 fprintf(s,D)
+ if round(i/20)==i/20
+        flushinput(s)
+        flushoutput(s)
+        end
+end
 %%
 fclose(s)
 delete(s)
